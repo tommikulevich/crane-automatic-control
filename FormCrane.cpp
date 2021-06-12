07fc0587845bd49808dd316cf331200d3ef83e98
Igor Malkovskiy <433garik433@gmail.com>
2021-06-12 18:59:59 +0200

    Add project files.

---
+++ FormCrane.cpp
@@ -0,0 +1,286 @@
+﻿// Plik naglowkowy z forma
+#include "FormCrane.h"
+
+// Przestrzeni nazw niezbedne do tworzenia i wywolania metod (funkcji skladowych) w tym pliku (cpp)
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]										// Ten atrybut musi byc obecny w punkcie wejscia (w tym przypadku funkcji main) dowolnej aplikacji Windows Forms
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();						// Wywolanie niezbednych metod do zarzadzania aplikacja
+	Application::SetCompatibleTextRenderingDefault(false);	// -//-
+
+	Project4::FormCrane form;								// Tworzenie zmiennej formularza (dalej - "formy") klasy FormWSA przestrzeni nazw Project3WSA
+	Application::Run(% form);								// Uruchamianie formy 
+}
+
+// Wspolrzedne
+struct OXY {
+	int X, Y;
+};
+
+
+OXY direction;		// Kierunek 
+OXY position;		// Polozenie
+OXY actionArea;		// Pole, na ktorym odbywa sie ruch
+
+// Konstruktor
+Project4::FormCrane::FormCrane(void)
+{
+	InitializeComponent();						// Ladowanie strony komponentow
+
+	actionArea.X = 330;
+	actionArea.Y = 480;
+
+	blocks = gcnew array<PictureBox^, 1> (3);	// Tworzenie tablicy blokow
+	blocks[0] = block1;
+	blocks[1] = block2;
+	blocks[2] = block3;
+}
+
+// Metoda startujaca 
+void Project4::FormCrane::NewSimulation()
+{
+	pictureBoxArea->Controls->Remove(hook);			// Usuwanie haka dzwigowego
+
+	hook = gcnew PictureBox();						// Tworzenie nowego haka oraz nadanie parametrow poczatkowych 
+	hook->Width = step;
+	hook->Height = step;
+	hook->Location = Point(0, 0);
+	hook->Image = Image::FromFile("bracing.jpg");
+
+	pictureBoxArea->Controls->Add(hook);			// Dodanie haka do pictureBoxArea
+
+	GenerateBlockPositions();
+
+	timer->Interval = updateInterval;
+	timer->Start();									// Start timer'a
+
+	direction.X = 1;								// Kierunek poczatkowy
+	direction.Y = 0;
+
+	work = true;
+}
+
+
+void Project4::FormCrane::GenerateBlockPositions()
+{
+	Random^ rand = gcnew Random();								// Zmienna losowa
+
+	for (int i = 0; i < 3; i++)									// Usuniecie wszystkich blokow z pictureBoxArea
+		pictureBoxArea->Controls->Remove(blocks[i]);			
+
+	for (int i = 0; i < 3; i++) {								// Cykl tworzenia trzech blokow i nadania parametrow poczatkowych
+		blocks[i] = gcnew PictureBox();								
+
+		blocks[i]->Width = step;
+		blocks[i]->Height = step;
+
+		position.Y = 450;
+
+		position.X = rand->Next(0, actionArea.X);				// Losowe polozenie po X (od 0 do actionArea.X = 330)
+
+		// Dodanie zdjecia do bloku oraz warunki, aby bloki nie nakladaly sie na siebie
+		if (i == 0) {
+			blocks[i]->Image = Image::FromFile("square.jpg");
+		}
+		else if (i == 1) {
+			blocks[i]->Image = Image::FromFile("triangle.jpg");
+
+			while ( (position.X <= blocks[i - 1]->Location.X + step && position.X >= blocks[i - 1]->Location.X - step) ) {
+				position.X = rand->Next(0, actionArea.X);
+			}
+		}
+		else {
+			blocks[i]->Image = Image::FromFile("circle.jpg");
+
+			while ((position.X <= blocks[i - 1]->Location.X + step && position.X >= blocks[i - 1]->Location.X - step) || (position.X <= blocks[i - 2]->Location.X + step && position.X >= blocks[i - 2]->Location.X - step)) {
+				position.X = rand->Next(0, actionArea.X);
+			}
+		}
+
+		int temp = position.X % step;							// Operacja, ktora pozwala otrzymac wspolrzedna podzielna przez skok (czyli 30)
+		position.X -= temp;
+
+		blocks[i]->Location = Point(position.X, position.Y);	// Nadanie polozenia
+
+		pictureBoxArea->Controls->Add(blocks[i]);				// Dodanie bloku do pictureBoxArea
+	}
+}
+
+// Przemieszczenie haka
+void Project4::FormCrane::MovementHook()
+{	
+	Pen^ redPen = gcnew Pen(Color::Black, 1.0F);			// Obiekt do rysowania linii i krzywych
+	Graphics^ thread = pictureBoxArea->CreateGraphics();	// Obiekt umozliwiajacy rysowanie w pictureBoxArea
+	
+	MovementDirection();
+
+	hook->Location = Point(hook->Location.X + direction.X * step, hook->Location.Y + direction.Y * step);	// Nadanie nowego polozenia
+
+	pictureBoxArea->Refresh();								// Odnowienie pola - ponownie narysowaс wszystkie elementy
+	thread->DrawLine(redPen, hook->Location.X + step/2, 0, hook->Location.X + step/2, hook->Location.Y);	// Rysowanie linki dzwigu
+}
+
+// Nadanie kierunku w zaleznosci od etapu 
+void Project4::FormCrane::MovementDirection()
+{
+	if (stage == 1 && hook->Location.X == blocks[1]->Location.X) {
+		direction.X = 0;
+		direction.Y = 1;
+
+		stage = 2;
+	}
+	else if (stage == 2 && hook->Location.Y + 30 == blocks[1]->Location.Y) {
+		direction.X = 0;
+		direction.Y = -1;
+
+		stage = 3;
+	}
+	else if (stage == 3 && hook->Location.Y == 240) {
+		if (hook->Location.X > blocks[0]->Location.X) {
+			direction.X = -1;
+			direction.Y = 0;
+
+			stage = 4;
+		}
+		else if (hook->Location.X < blocks[0]->Location.X) {
+			direction.X = 1;
+			direction.Y = 0;
+
+			stage = 4;
+		}
+		else if (hook->Location.X == blocks[0]->Location.X) {
+			direction.X = 0;
+			direction.Y = 1;
+
+			stage = 4;
+		}
+	}
+	else if (stage == 4 && hook->Location.X == blocks[0]->Location.X) {
+		direction.X = 0;
+		direction.Y = 1;
+
+		stage = 5;
+	}
+	else if (stage == 5 && hook->Location.Y + 60 == blocks[0]->Location.Y) {
+		direction.X = 0;
+		direction.Y = -1;
+
+		stage = 6;
+	}
+	else if (stage == 6 && hook->Location.Y == 240) {
+		if (hook->Location.X > blocks[2]->Location.X) {
+			direction.X = -1;
+			direction.Y = 0;
+
+			stage = 7;
+		}
+		else if (hook->Location.X < blocks[2]->Location.X) {
+			direction.X = 1;
+			direction.Y = 0;
+
+			stage = 7;
+		}
+		else if (hook->Location.X == blocks[2]->Location.X) {
+			direction.X = 0;
+			direction.Y = 1;
+
+			stage = 7;
+		}
+	}
+	else if (stage == 7 && hook->Location.X == blocks[2]->Location.X) {
+		direction.X = 0;
+		direction.Y = 1;
+
+		stage = 8;
+	}
+	else if (stage == 8 && hook->Location.Y + 30 == blocks[2]->Location.Y) {
+		direction.X = 0;
+		direction.Y = -1;
+
+		stage = 9;
+	}
+	else if (stage == 9 && hook->Location.Y == 240) {
+		if (hook->Location.X > blocks[0]->Location.X) {
+			direction.X = -1;
+			direction.Y = 0;
+
+			stage = 10;
+		}
+		else if (hook->Location.X < blocks[0]->Location.X) {
+			direction.X = 1;
+			direction.Y = 0;
+
+			stage = 10;
+		}
+		else if (hook->Location.X == blocks[0]->Location.X) {
+			direction.X = 0;
+			direction.Y = 1;
+
+			stage = 10;
+		}
+	}
+	else if (stage == 10 && hook->Location.X == blocks[0]->Location.X) {
+		direction.X = 0;
+		direction.Y = 1;
+
+		stage = 11;
+	}
+	else if (stage == 11 && hook->Location.Y + 90 == blocks[0]->Location.Y) {
+		direction.X = 0;
+		direction.Y = 0;
+
+		work = false;
+	}
+}
+
+// Przemieszczenie blokow w zaleznosci od etapu 
+void Project4::FormCrane::MovementBlocks()
+{
+	switch (stage) 
+	{
+	case 3 : 
+		blocks[1]->Location = Point(blocks[1]->Location.X + direction.X * step, blocks[1]->Location.Y + direction.Y * step);
+		break;
+	case 4 :
+		blocks[1]->Location = Point(blocks[1]->Location.X + direction.X * step, blocks[1]->Location.Y + direction.Y * step);
+		break;
+	case 5 :
+		blocks[1]->Location = Point(blocks[1]->Location.X + direction.X * step, blocks[1]->Location.Y + direction.Y * step);
+		break;
+	case 9 :
+		blocks[2]->Location = Point(blocks[2]->Location.X + direction.X * step, blocks[2]->Location.Y + direction.Y * step);
+		break;
+	case 10 :
+		blocks[2]->Location = Point(blocks[2]->Location.X + direction.X * step, blocks[2]->Location.Y + direction.Y * step);
+		break;
+	case 11 :
+		blocks[2]->Location = Point(blocks[2]->Location.X + direction.X * step, blocks[2]->Location.Y + direction.Y * step);
+		break;
+	default :
+		break;
+	}
+}
+
+// Metoda startujaca - po nacisnieciu przycisku buttonPrint
+System::Void Project4::FormCrane::buttonBlock_Click(System::Object^ sender, System::EventArgs^ e)
+{	
+	stage = 1;
+	pictureBoxArea->Refresh();	
+
+	NewSimulation();
+}
+
+// Metoda ktora wywoluje sie co 350 milisekundy (event timer'a)
+void Project4::FormCrane::FormCrane_Update(Object^ object, EventArgs^ e)
+{
+	if (work) {
+		MovementHook();
+		MovementBlocks();
+	}
+	else {
+		timer->Stop();								// Zatrzymac timer
+		MessageBox::Show("Zrobione!", "Wow!");
+	}
+}
---
+++ FormCrane.h
@@ -0,0 +1,128 @@
+﻿#pragma once
+
+namespace Project4 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for FormCrane
+	/// </summary>
+	public ref class FormCrane : public System::Windows::Forms::Form
+	{
+	public:
+		FormCrane(void);
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~FormCrane()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::PictureBox^ pictureBoxArea;
+	private: System::Windows::Forms::Button^ buttonGo;
+	protected:
+
+
+	private: System::Windows::Forms::Timer^ timer;
+	private: System::ComponentModel::IContainer^ components;
+
+
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(FormCrane::typeid));
+			this->pictureBoxArea = (gcnew System::Windows::Forms::PictureBox());
+			this->buttonGo = (gcnew System::Windows::Forms::Button());
+			this->timer = (gcnew System::Windows::Forms::Timer(this->components));
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxArea))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBoxArea
+			// 
+			this->pictureBoxArea->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBoxArea->Location = System::Drawing::Point(300, 109);
+			this->pictureBoxArea->Name = L"pictureBoxArea";
+			this->pictureBoxArea->Size = System::Drawing::Size(330, 480);
+			this->pictureBoxArea->TabIndex = 0;
+			this->pictureBoxArea->TabStop = false;
+			// 
+			// buttonGo
+			// 
+			this->buttonGo->Location = System::Drawing::Point(565, 10);
+			this->buttonGo->Name = L"buttonGo";
+			this->buttonGo->Size = System::Drawing::Size(57, 20);
+			this->buttonGo->TabIndex = 1;
+			this->buttonGo->Text = L"Go!";
+			this->buttonGo->UseVisualStyleBackColor = true;
+			this->buttonGo->Click += gcnew System::EventHandler(this, &FormCrane::buttonBlock_Click);
+			// 
+			// timer
+			// 
+			this->timer->Interval = 350;
+			this->timer->Tick += gcnew System::EventHandler(this, &FormCrane::FormCrane_Update);
+			// 
+			// FormCrane
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(640, 591);
+			this->Controls->Add(this->buttonGo);
+			this->Controls->Add(this->pictureBoxArea);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
+			this->MaximizeBox = false;
+			this->Name = L"FormCrane";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Projekt 4 - Wizualizacja Systemów Automatyki";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxArea))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+
+	private: array<PictureBox^, 1>^ blocks;		// Tworzenie tablicy blokow
+	private: PictureBox^ block1;				// Zmienne blokow typu pictureBox
+	private: PictureBox^ block2;
+	private: PictureBox^ block3;
+	private: PictureBox^ hook;					// Zmienne haka typu pictureBox
+	private: int step = 30;						// Krok
+	private: int stage;							// Etap
+	private: bool work;							// Dzialanie programu (true - kontynuj, false - zakoncz)
+	private: float updateInterval = 350;		// Interwal odswierzania (wyrazony w milisekundach)
+
+	private: void NewSimulation();
+	private: void GenerateBlockPositions();
+	private: void MovementHook();
+	private: void MovementDirection();
+	private: void MovementBlocks();
+
+	private: System::Void buttonBlock_Click(System::Object^ sender, System::EventArgs^ e);
+
+	private: void FormCrane_Update(Object^ object, EventArgs^ e);
+	};
+}
